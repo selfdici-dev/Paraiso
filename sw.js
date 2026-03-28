651257ac6f480d8bb3baf90f76510c5071da5abc
selfdici-dev <selfdici@gmail.com>
2026-03-28 21:33:06 +1000

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE = "become-pwa-v1";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./sw.js"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
